44fffe41996c09e868da8e00dd8abf634ff69b24
Shawn Mathes <shawnmathes@gmail.com>
2014-03-03 19:29:36 -0600

    Update to use the expanded logging interface and for FileLogging to use its own method for appending to files.

--- src/edusystemplugin/fileandconsoleloggingextension/ConsoleLogging.java
+++ src/edusystemplugin/fileandconsoleloggingextension/ConsoleLogging.java
@@ -5,13 +5,32 @@ import edusystemplugin.extensions.IEduLogging;
 public class ConsoleLogging implements IEduLogging {
 
 	public ConsoleLogging() {
-		// TODO Auto-generated constructor stub
+
 	}
 
 	@Override
 	public void writeLog(String message) {
-		// TODO Auto-generated method stub
 		System.out.println(message);
+	}
+
+	@Override
+	public void writeDebug(String message) {
+		System.out.println("DEBUG: " + message);
+	}
+
+	@Override
+	public void writeInfo(String message) {
+		System.out.println("INFO:  " + message);
+	}
+
+	@Override
+	public void writeError(String message) {
+		System.out.println("ERROR: " + message);
+	}
+
+	@Override
+	public void writeAudit(String message) {
+		System.out.println("AUDIT: " + message);
 	}
 
 }
--- src/edusystemplugin/fileandconsoleloggingextension/FileLogging.java
+++ src/edusystemplugin/fileandconsoleloggingextension/FileLogging.java
@@ -1,19 +1,88 @@
 package edusystemplugin.fileandconsoleloggingextension;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import edusystemplugin.extensions.IEduLogging;
 
 public class FileLogging implements IEduLogging {
 
 	private static String logfile = Configuration.getDataRoot()
 			+ "edusystem.log";
-	
+	private static String auditlog = Configuration.getDataRoot()
+			+ "edusystem-audit.log";
+
 	public FileLogging() {
-		
+
 	}
 
 	@Override
 	public void writeLog(String message) {
-		WriteFile writefile = new WriteFile();
-		writefile.writeString(message, logfile);
+		fileAppend(getDate() + " " + message, logfile);
+	}
+
+	@Override
+	public void writeDebug(String message) {
+		fileAppend(getDate() + " " + message, logfile);
+	}
+
+	@Override
+	public void writeInfo(String message) {
+		fileAppend(getDate() + " " + message, logfile);
+	}
+
+	@Override
+	public void writeError(String message) {
+		fileAppend(getDate() + " " + message, logfile);
+	}
+
+	@Override
+	public void writeAudit(String message) {
+		fileAppend(getDate() + " " + message, auditlog);
+	}
+
+	private String getDate() {
+		Date currentDate = new Date();
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(
+				"yyyy-MM-dd HH:mm:ss");
+		return simpleDateFormat.format(currentDate);
+	}
+
+	private void fileAppend(String text, String filepath) {
+		  PrintWriter out = null;
+		  BufferedWriter bw = null;
+		  FileWriter fw = null;
+		  try{
+		     fw = new FileWriter(filepath, true);
+		     bw = new BufferedWriter(fw);
+		     out = new PrintWriter(bw);
+		     out.println(text);
+		  }
+		  catch( IOException e ){
+		     // File writing/opening failed at some stage.
+		  }
+		  finally{
+		     try{
+		        if( out != null ){
+		           out.close(); // Will close bw and fw too
+		        }
+		        else if( bw != null ){
+		           bw.close(); // Will close fw too
+		        }
+		        else if( fw != null ){
+		           fw.close();
+		        }
+		        else{
+		           // Oh boy did it fail hard! :3
+		        }
+		     }
+		     catch( IOException e ){
+		        // Closing the file writers failed for some obscure reason
+		     }
+		  }
 	}
 }
